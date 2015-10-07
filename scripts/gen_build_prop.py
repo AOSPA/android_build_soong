3712db9e50b8bc965d3cc61e82bd917b66cde80d
beanstown106 <nbenis106@gmail.com>
2015-10-07 04:31:15 -0400

    soong: Show only Build ID in "Build number" section
    (Former Change-Id: Ibf29628c622cab05597a5ca8cfd265534d303f8c)
    Change-Id: I3e4f83d7ca5f0ac3b2036c5ef363c09ad565fcd1
    Signed-off-by: Hridaya Prajapati <hridaya@pixelos.net>

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -220,12 +220,12 @@ def generate_build_info(args):
 
     # Dev. branches should have DISPLAY_BUILD_NUMBER set
     if config["DisplayBuildNumber"]:
-      print(f"ro.build.display.id?={config['BuildId']}.{config['BuildNumber']} {config['BuildKeys']}")
+      print(f"ro.build.display.id?={config['BuildId']}")
     else:
-      print(f"ro.build.display.id?={config['BuildId']} {config['BuildKeys']}")
+      print(f"ro.build.display.id?={config['BuildId']}")
   else:
     # Non-user builds should show detailed build information (See build desc above)
-    print(f"ro.build.display.id?={config['BuildDesc']}")
+    print(f"ro.build.display.id?={config['BuildId']}")
   print(f"ro.build.version.incremental={config['BuildNumber']}")
   print(f"ro.build.version.sdk={config['Platform_sdk_version']}")
   print(f"ro.build.version.sdk_full={config['Platform_sdk_version_full']}")
