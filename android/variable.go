3d4df7337d4ea58a2d0a8ce4c7670dabeb7b15a0
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-31 20:00:40 -0700

    soong: Add function to return camera parameters library name
    * Modified for Paranoid Android by herna
    
    Change-Id: I3113ea6813b82c3f6b250205080030ff9c2255ec
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>

--- android/variable.go
+++ android/variable.go
@@ -169,6 +169,9 @@ type productVariables struct {
 	DeviceKernelHeaders []string `json:",omitempty"`
 
 	BoardUsesQTIHardware *bool `json:",omitempty"`
+
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"
 }
 
 func boolPtr(v bool) *bool {
