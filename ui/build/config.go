ec18306d4df1542dad655e7d45aef49086bb959b
Josh Choo <dev@joshchoo.com>
2018-12-01 04:02:03 +0000

    Revert "soong: Enforce absolute path if OUT_DIR is set"
    This reverts commit bdd68ce327a39832eda96aa3136cd210e2d72cba.

--- ui/build/config.go
+++ ui/build/config.go
@@ -65,11 +65,7 @@ func NewConfig(ctx Context, args ...string) Config {
 
 	// Make sure OUT_DIR is set appropriately
 	if outDir, ok := ret.environ.Get("OUT_DIR"); ok {
-		outDir := filepath.Clean(outDir)
-		if (!filepath.IsAbs(outDir)) {
-			outDir = filepath.Join(os.Getenv("TOP"), outDir)
-		}
-		ret.environ.Set("OUT_DIR", outDir)
+		ret.environ.Set("OUT_DIR", filepath.Clean(outDir))
 	} else {
 		outDir := "out"
 		if baseDir, ok := ret.environ.Get("OUT_DIR_COMMON_BASE"); ok {
