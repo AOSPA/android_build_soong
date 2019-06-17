1614f941cb5dc6f5a2a743d64346a79bd5dc0536
David Ng <dave@codeaurora.org>
2019-06-17 13:48:54 -0700

    Re-enable "Do not re-export _Unwind_XXX symbols."
    Need to drop _Unwind_* symbols re-export such that binaries
    are properly, statically linked against libgcc.a instead of
    relying on other shared libraries providing such symbols.
    
    This reverts commit 800c1b8ad05ffcf09010e72331b7a1d6e100c6bb.
    
    CRs-Fixed: 2474069
    Change-Id: Ibaa7251dc166e12fc038bdd909efaafae36cc42b

--- cc/config/global.go
+++ cc/config/global.go
@@ -90,6 +90,7 @@ var (
 		"-Wl,--warn-shared-textrel",
 		"-Wl,--fatal-warnings",
 		"-Wl,--no-undefined-version",
+		"-Wl,--exclude-libs,libgcc.a",
 	}
 
 	deviceGlobalLldflags = append(ClangFilterUnknownLldflags(deviceGlobalLdflags),
