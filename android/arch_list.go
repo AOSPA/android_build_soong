5964b5d604a55724a2a5c036c5ee98095d18b1c6
Jake Weinstein <jake@aospa.co>
2019-07-31 10:53:24 -0500

    Define AArch32 soong variable
    Change-Id: I1a52a8e8e436eec827022413270d8b7c76808a69
    (cherry picked from commit a677cdce6f6678e1c3cf654da1921f4ce1c951ff)
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>

--- android/arch_list.go
+++ android/arch_list.go
@@ -99,6 +99,7 @@ var cpuVariants = map[ArchType][]string{
 
 var archFeatures = map[ArchType][]string{
 	Arm: {
+		"aarch32",
 		"neon",
 	},
 	Arm64: {
@@ -135,9 +136,11 @@ var androidArchFeatureMap = map[ArchType]map[string][]string{
 			"neon",
 		},
 		"armv8-a": {
+			"aarch32",
 			"neon",
 		},
 		"armv8-2a": {
+			"aarch32",
 			"neon",
 		},
 	},
