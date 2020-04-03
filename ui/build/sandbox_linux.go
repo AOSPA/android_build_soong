775a5777912389cfa10903924b86c190e59dbb36
Diego Wilson <diegowilson@google.com>
2020-04-03 18:29:13 +0000

    Revert "Temporarily disable nsjail"
    This reverts commit 08828f5e691918a33fcd495a8703ef6caa1fe845.
    
    Fixes: 151650800
    Change-Id: Idfa72259e147f610beef886474b6e7e5f5c91203

--- ui/build/sandbox_linux.go
+++ ui/build/sandbox_linux.go
@@ -33,14 +33,14 @@ type Sandbox struct {
 var (
 	noSandbox    = Sandbox{}
 	basicSandbox = Sandbox{
-		Enabled: false,
+		Enabled: true,
 	}
 
 	dumpvarsSandbox = basicSandbox
 	katiSandbox     = basicSandbox
 	soongSandbox    = basicSandbox
 	ninjaSandbox    = Sandbox{
-		Enabled:              false,
+		Enabled:              true,
 		DisableWhenUsingGoma: true,
 
 		AllowBuildBrokenUsesNetwork: true,
