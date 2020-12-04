e161889c4cee58ff25f1c8a29ed3bb441ee418fb
Evan Anderson <evananderson@aospa.co>
2020-12-03 19:32:20 -0600

    global: Don't fail if SDClang variables aren't defined
    CAF assumes that SDClang will always be in the environment, but
    there are cases where executing soong is done without the variables,
    and thus causes startup failures. Workaround this by checking for
    SDCLANG_CONFIG to be defined and by checking if SDClang is enabled
    before failing due to SDCLANG_PATH being empty.
    
    Change-Id: I193815fb957d8830f0e39f1e8dd9b53eac47b60c

--- cc/config/global.go
+++ cc/config/global.go
@@ -647,6 +647,11 @@ func setSdclangVars() {
 	sdclangConfigPath := os.Getenv("SDCLANG_CONFIG")
 	sdclangSA := os.Getenv("SDCLANG_SA_ENABLED")
 
+	// Bail out if SDCLANG_CONFIG isn't set
+	if sdclangConfigPath == "" {
+		return
+	}
+
 	type sdclangAEConfig struct {
 		SDCLANG_AE_FLAG string
 	}
@@ -733,7 +738,7 @@ func setSdclangVars() {
 	}
 
 	// Sanity check SDCLANG_PATH
-	if envPath := os.Getenv("SDCLANG_PATH"); sdclangPath == "" && envPath == "" {
+	if envPath := os.Getenv("SDCLANG_PATH"); SDClang && sdclangPath == "" && envPath == "" {
 		panic("SDCLANG_PATH can not be empty")
 	}
 
