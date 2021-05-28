950864d7d9c1dce8c8f6830e69b3844e851fa4e5
Vishalcj17 <vishalcj@aospa.co>
2021-05-28 19:42:01 +0530

    Revert "[pgo] Sampling PGO does not require benchmark property"
    This reverts commit 64471866d5312d5a664f98ce24c21cbf286ad9d0.
    
    Change-Id: Ice2802d3348bc409b28f141cd9d06df860b73780

--- cc/pgo.go
+++ cc/pgo.go
@@ -199,8 +199,8 @@ func (props *PgoProperties) isPGO(ctx BaseModuleContext) bool {
 		return false
 	}
 
-	// profileKindPresent and filePresent are mandatory properties.
-	if !profileKindPresent || !filePresent {
+	// If at least one property exists, validate that all properties exist
+	if !profileKindPresent || !filePresent || !benchmarksPresent {
 		var missing []string
 		if !profileKindPresent {
 			missing = append(missing, "profile kind (either \"instrumentation\" or \"sampling\" property)")
@@ -208,13 +208,11 @@ func (props *PgoProperties) isPGO(ctx BaseModuleContext) bool {
 		if !filePresent {
 			missing = append(missing, "profile_file property")
 		}
+		if !benchmarksPresent {
+			missing = append(missing, "non-empty benchmarks property")
+		}
 		missingProps := strings.Join(missing, ", ")
 		ctx.ModuleErrorf("PGO specification is missing properties: " + missingProps)
-	}
-
-	// Benchmark property is mandatory for instrumentation PGO.
-	if isInstrumentation && !benchmarksPresent {
-		ctx.ModuleErrorf("Instrumentation PGO specification is missing benchmark property")
 	}
 
 	if isSampling && isInstrumentation {
