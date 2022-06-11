abe85098a7df0b56057d0b6581a9edf4576f907f
Juhyung Park <qkrwngud825@gmail.com>
2022-06-12 02:45:42 +0900

    build: soong: use jemalloc by default and allow opt-in to scudo (2/4)
    Also, clean up broken DisableScudo/PRODUCT_DISABLE_SCUDO.
    
    After these changes, malloc_not_svelte is only used to override cflags
    in external/jemalloc_new. Change accordingly.
    
    Detailed reason for switching to jemalloc by default is written in the
    bionic commit.
    
    Change-Id: Id5afcf064a5db6011676dc7141c71fd5bb0007cb
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -70,13 +70,20 @@ type variableProperties struct {
 			Enabled *bool `android:"arch_variant"`
 		} `android:"arch_variant"`
 
-		Malloc_not_svelte struct {
+		Malloc_use_scudo struct {
 			Cflags              []string `android:"arch_variant"`
 			Shared_libs         []string `android:"arch_variant"`
 			Whole_static_libs   []string `android:"arch_variant"`
 			Exclude_static_libs []string `android:"arch_variant"`
 			Srcs                []string `android:"arch_variant"`
 			Header_libs         []string `android:"arch_variant"`
+		} `android:"arch_variant"`
+
+		Malloc_not_svelte struct {
+			Cflags              []string `android:"arch_variant"`
+			Shared_libs         []string `android:"arch_variant"`
+			Whole_static_libs   []string `android:"arch_variant"`
+			Exclude_static_libs []string `android:"arch_variant"`
 		} `android:"arch_variant"`
 
 		Malloc_zero_contents struct {
@@ -302,6 +309,7 @@ type productVariables struct {
 	Unbundled_build_sdks_from_source *bool    `json:",omitempty"`
 	Always_use_prebuilt_sdks         *bool    `json:",omitempty"`
 	Skip_boot_jars_check             *bool    `json:",omitempty"`
+	Malloc_use_scudo                 *bool    `json:",omitempty"`
 	Malloc_not_svelte                *bool    `json:",omitempty"`
 	Malloc_zero_contents             *bool    `json:",omitempty"`
 	Malloc_pattern_fill_contents     *bool    `json:",omitempty"`
@@ -339,8 +347,6 @@ type productVariables struct {
 	EnableCFI       *bool    `json:",omitempty"`
 	CFIExcludePaths []string `json:",omitempty"`
 	CFIIncludePaths []string `json:",omitempty"`
-
-	DisableScudo *bool `json:",omitempty"`
 
 	MemtagHeapExcludePaths      []string `json:",omitempty"`
 	MemtagHeapAsyncIncludePaths []string `json:",omitempty"`
@@ -564,6 +570,7 @@ func (v *productVariables) SetDefaultConfig() {
 		AAPTCharacteristics: stringPtr("nosdcard"),
 		AAPTPrebuiltDPI:     []string{"xhdpi", "xxhdpi"},
 
+		Malloc_use_scudo:             boolPtr(false),
 		Malloc_not_svelte:            boolPtr(true),
 		Malloc_zero_contents:         boolPtr(true),
 		Malloc_pattern_fill_contents: boolPtr(false),
