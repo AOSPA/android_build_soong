4522703e814012cb6c6714090ae3236d5cbee948
HeroBuxx <me@herobuxx.me>
2023-10-06 20:01:31 +0000

    soong: ui: dumpvar: Apparently `PRODUCT_SOONG_NAMESPACES` is necessary
    Bring back PRODUCT_SOONG_NAMESPACES after this change:
    3690485a168e0c80ebbfb6c737f6f93281fe8a70
    
    Change-Id: I1efa7620d79eb9c330f0f028a91e10d2a9ce49e8
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -199,6 +199,7 @@ var BannerVars = []string{
 	"BUILD_ID",
 	"OUT_DIR",
 	"SOONG_SDK_SNAPSHOT_TARGET_BUILD_RELEASE",
+	"PRODUCT_SOONG_NAMESPACES",
 }
 
 func Banner(config Config, make_vars map[string]string) string {
@@ -317,7 +318,6 @@ func runMakeProductConfig(ctx Context, config Config) {
 		"HOST_CROSS_ARCH",
 		"HOST_CROSS_2ND_ARCH",
 		"HOST_BUILD_TYPE",
-		"PRODUCT_SOONG_NAMESPACES",
 
 		"DEFAULT_WARNING_BUILD_MODULE_TYPES",
 		"DEFAULT_ERROR_BUILD_MODULE_TYPES",
