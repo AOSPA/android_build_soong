4da9470c7e2bd3f542173f8ae20ec80cc51cb58f
Michael Bestas <mkbestas@lineageos.org>
2024-10-30 19:53:42 +0200

    Conditionally use Unix epoch time for build incremental
    Incremental version is used in various places to invalidate caches.
    Setting BUILD_NUMBER directly causes unnecessary rebuilds due to
    environment variable changes, so simply set it to Unix epoch unless
    BUILD_NUMBER is explicitly set.
    
    Change-Id: Id590df48ae1b73b63039f185644103d66a4bbbb3

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -118,6 +118,9 @@ def parse_args():
   if args.build_thumbprint_file:
     config["BuildThumbprint"] = args.build_thumbprint_file.read().strip()
 
+  if config["BuildNumber"].startswith("eng."):
+    config["BuildNumber"] = config["DateUtc"]
+
   override_config(config)
 
   append_additional_system_props(args)
